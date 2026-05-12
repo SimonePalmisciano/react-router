2cef71bf803eb09b688e77693eb8cfa415b59232
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-05-12 17:09:05 +0200

    creata la base della pagina della homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <main>
+            <div className="d-flex justify-content-center">
+                <h1 className="m-3">Questa è la tua HomePage</h1>
+            </div>
+            <section className="main-section banner-img">
+                <div className="container d-flex flex-column justify-content-center align-items-center h-100">
+                    <h3 className="mb-3">Qui puoi trovare tutti i tuoi prodotti del mondo</h3>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Quas debitis ipsum voluptatibus expedita, error, veniam rem doloremque quos quae,
+                        odit voluptatum dolores? Ab quod cupiditate possimus nisi ex blanditiis accusamus.
+                    </p>
+                </div>
+            </section>
+        </main>
+
     )
 }
 
